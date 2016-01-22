48bceb71714ef25a72bfd5a4396a794e43936d17
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-01-22 14:36:16 +0000

    Standardized the layout of the files

--- bin/run.sh
+++ bin/run.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env ruby
 
 ##Setup Load Path
-$LOAD_PATH << File.expand_path("../../lib", __FILE__)
+$LOAD_PATH << File.expand_path("../../", __FILE__)
 
 require 'sinatra'
 require 'tic_tac_toe_web_app'
 
-TicTacToeWebApp.run!
+#TicTacToeWebApp.run!
+
+ruby lib/tic_tac_toe_web_app.rb
