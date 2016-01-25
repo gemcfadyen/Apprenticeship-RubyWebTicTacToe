deb99ffd26885378c40682701289326508579a45
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-01-25 11:12:52 +0000

    Landing page loads correctly and displays empty grid

--- tic_tac_toe_web_app.rb
+++ tic_tac_toe_web_app.rb
@@ -4,24 +4,11 @@ require 'sinatra/base'
 $LOAD_PATH << File.expand_path("../lib", __FILE__)
 require 'board'
 require 'player_symbols'
+require 'web_board_displayer'
 
 get '/' do
-  board = Board.new
-
-  rows = board.grid_for_display
-  dimension = 3 
-  #transform rows to numbers
-  @formatted_rows = rows.map.with_index do |row, o|
-    p "index of outer rows is: " + o.to_s
-    offset = dimension * o
-    p "offset is : "  + offset.to_s
-    formatted_cells =  row.map.with_index do  |cell, i| 
-      cell.nil? ? i+1 + offset : cell.to_s
-    end
-  end
-
-  p "for displaying " +  @formatted_rows.to_s
-  @valid_moves = [PlayerSymbols::X, PlayerSymbols::O]
+  @formatted_rows = WebBoardDisplayer::new.format(Board.new)
+  @valid_moves = PlayerSymbols::all
   erb :landing_page
 end
 
@@ -37,43 +24,44 @@ get '/next_move' do
 
   # create a new board from this, update it
   #
+  p "contents of board is: " +  contents_of_board.to_s
   grid = contents_of_board.delete("[").delete("]").delete(" ").split(",").map do |n|
     if is_number(n)
       nil 
     else
-     n
+      n
     end
 
-    end
+  end
 
-    board =  Board.new(grid)
-    new_board = board.make_move(@move.to_i - 1, PlayerSymbols::X)
-    p "Updated board is : " + new_board.grid_for_display.to_s
-    dimension = 3
-    @formatted_rows = new_board.grid_for_display.map.with_index do |row, o|
-      p "index of outer rows is: " + o.to_s
-      offset = dimension * o
-      p "offset is : "  + offset.to_s
-      formatted_cells =  row.map.with_index do  |cell, i| 
-        cell.nil? ? i+1 + offset : cell.to_s
-      end
-    end
-
-    p "for displaying " +  @formatted_rows.to_s
-    @valid_moves = [PlayerSymbols::X.to_s, PlayerSymbols::O.to_s]
-
-    if @move == "1" 
-      @game_status = "Winner!" 
-      erb :landing_page
-    else 
-      erb :landing_page
+  board =  Board.new(grid)
+  new_board = board.make_move(@move.to_i - 1, PlayerSymbols::X)
+  p "Updated board is : " + new_board.grid_for_display.to_s
+  dimension = 3
+  @formatted_rows = new_board.grid_for_display.map.with_index do |row, o|
+    p "index of outer rows is: " + o.to_s
+    offset = dimension * o
+    p "offset is : "  + offset.to_s
+    formatted_cells =  row.map.with_index do  |cell, i| 
+      cell.nil? ? i+1 + offset : cell.to_s
     end
   end
 
-  def is_number(value)
-    Integer(value)
-    return true
+  p "for displaying " +  @formatted_rows.to_s
+  @valid_moves = [PlayerSymbols::X.to_s, PlayerSymbols::O.to_s]
 
-  rescue
-    return false
+  if @move == "1" 
+    @game_status = "Winner!" 
+    erb :landing_page
+  else 
+    erb :landing_page
   end
+end
+
+def is_number(value)
+  Integer(value)
+  return true
+
+rescue
+  return false
+end
