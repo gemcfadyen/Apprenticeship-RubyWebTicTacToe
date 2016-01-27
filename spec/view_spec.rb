706138a547ece99162c6aba5c509a09c7c794ab6
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-01-27 17:36:05 +0000

    When board has occupied cells, there are less links

--- spec/view_spec.rb
+++ spec/view_spec.rb
@@ -7,16 +7,34 @@ RSpec.describe "ERB Views" do
 
   it "landing page with empty grid contains 9 links" do
     @formatted_rows = Board.new.grid_for_display
-    p @formatted_rows.to_s
-    @valid_moves = PlayerSymbols::all
+    @valid_moves = PlayerSymbols::all.map {|i| i.to_s}
     @game_status = nil
 
-    template = ERB.new File.new("views/landing_page.erb").read
+    template = load_template
     rendered = template.result(binding())
 
-    html_doc = Nokogiri::HTML(rendered)
-    links = html_doc.css("a") # finds links on the page
+    links = count_ahref_links(rendered)
     expect(links.length).to eq 9
   end
-end
 
+  it "landing page only has links for unoccupied cells" do
+    @formatted_rows = Board.new([PlayerSymbols::X, nil, nil, PlayerSymbols::O, nil, nil, PlayerSymbols::O, nil, nil]).grid_for_display
+    @valid_moves = PlayerSymbols::all.map {|i| i.to_s}
+    @game_status = nil
+
+    template = load_template
+    rendered = template.result(binding())
+
+    links = count_ahref_links(rendered)
+    expect(links.length).to eq 6
+  end
+
+  def load_template
+    ERB.new File.new("views/landing_page.erb").read
+  end
+
+  def count_ahref_links(rendered)
+    html_doc = Nokogiri::HTML(rendered)
+    html_doc.css("a")
+  end
+end
