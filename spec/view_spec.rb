875c0b714f61a84b43395e5f83caaa6d90fdb1fb
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-01-28 11:18:17 +0000

    Unit tests for the view logic added

--- spec/view_spec.rb
+++ spec/view_spec.rb
@@ -8,23 +8,30 @@ RSpec.describe "ERB Views" do
   it "landing page with empty grid contains 9 links" do
     @formatted_rows = Board.new.grid_for_display
     @valid_moves = PlayerSymbols::all.map {|i| i.to_s}
-    @game_status = nil
 
-    rendered = load_template.result(binding())
-    html_doc = transform_to_html(rendered)
-    links = count_ahref_links(html_doc)
-    expect(links.length).to eq 9
+    html_doc = transform_to_html(load_template.result(binding()))
+
+    expect(count_ahref_links(html_doc).length).to eq 9
   end
 
-  it "landing page only has links for unoccupied cells" do
+  it "landing page only shows links for unoccupied cells" do
     @formatted_rows = Board.new([PlayerSymbols::X, nil, nil, PlayerSymbols::O, nil, nil, PlayerSymbols::O, nil, nil]).grid_for_display
     @valid_moves = PlayerSymbols::all.map {|i| i.to_s}
-    @game_status = nil
 
-    rendered = load_template.result(binding())
-    html_doc = transform_to_html(rendered)
-    links = count_ahref_links(html_doc)
-    expect(links.length).to eq 6
+    html_doc = transform_to_html(load_template.result(binding()))
+
+    expect(count_ahref_links(html_doc).length).to eq 6
+  end
+
+  it "landing page shows moves made" do
+    @formatted_rows = Board.new([PlayerSymbols::X, nil, nil, PlayerSymbols::O, nil, nil, PlayerSymbols::O, nil, nil]).grid_for_display
+    @valid_moves = PlayerSymbols::all.map {|i| i.to_s}
+
+    html_doc = transform_to_html(load_template.result(binding()))
+    table_entries = all_table_entries(html_doc)
+
+    expect(count_number_of("O", table_entries)).to eq 2
+    expect(count_number_of("X", table_entries)).to eq 1
   end
 
   it "landing page has no links when game is won" do
@@ -32,22 +39,31 @@ RSpec.describe "ERB Views" do
     @valid_moves = PlayerSymbols::all.map {|i| i.to_s}
     @game_status = "Winner"
 
-    rendered = load_template.result(binding())
-    html_doc = transform_to_html(rendered)
+    html_doc = transform_to_html(load_template.result(binding()))
 
-    links = count_ahref_links(html_doc)
-    expect(links.length).to eq 0
+    expect(count_ahref_links(html_doc).length).to eq 0
   end
 
-  it "landing page shows winning message when game is won" do
-    @formatted_rows = Board.new([PlayerSymbols::X, PlayerSymbols::X, PlayerSymbols::X, PlayerSymbols::O, nil, nil, PlayerSymbols::O, nil, nil]).grid_for_display
+  it "landing page shows game status when it is set" do
+    @formatted_rows = Board.new([PlayerSymbols::X, PlayerSymbols::O, PlayerSymbols::X, PlayerSymbols::O, PlayerSymbols::O, PlayerSymbols::X, PlayerSymbols::X, PlayerSymbols::X, PlayerSymbols::O]).grid_for_display
     @valid_moves = PlayerSymbols::all.map {|i| i.to_s}
-    @game_status = "Winner"
+    @game_status = "Draw"
 
-    rendered = load_template.result(binding())
-    html_doc = transform_to_html(rendered)
-    all_paragraphs = html_doc.css("p")[1].text
-    expect(all_paragraphs).to eq "Winner"
+    html_doc = transform_to_html(load_template.result(binding()))
+    game_status_displayed = html_doc.css("p")[1].text
+
+    expect(game_status_displayed).to eq "Draw"
+  end
+
+  it "landing page shows no status when game status is unset" do
+    @formatted_rows = Board.new([nil, nil, nil, PlayerSymbols::O, PlayerSymbols::O, PlayerSymbols::X, PlayerSymbols::X, PlayerSymbols::X, PlayerSymbols::O]).grid_for_display
+    @valid_moves = PlayerSymbols::all.map {|i| i.to_s}
+    @game_status = nil
+
+    html_doc = transform_to_html(load_template.result(binding()))
+    game_status_displayed = html_doc.css("p")[1].text
+
+    expect(game_status_displayed).to eq ""
   end
 
   def load_template
@@ -60,5 +76,13 @@ RSpec.describe "ERB Views" do
 
   def count_ahref_links(html)
     html.css("a")
+  end
+
+  def all_table_entries(html)
+    html.css("td").text
+  end
+
+  def count_number_of(symbol, table_entries)
+    table_entries.count(symbol)
   end
 end
